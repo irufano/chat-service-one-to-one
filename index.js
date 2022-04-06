215caefbf48a4e1f03c8218338b2f0c3c0ed4548
irufano <irfanhidayatms@gmail.com>
2022-04-06 14:53:27 +0700

    setup socket.io

---
+++ index.js
@@ -0,0 +1,41 @@
+const app = require("express")();
+const server = require("http").createServer(app);
+const socketIO = require("socket.io")(server);
+
+app.get("/", (req, res) => {
+  res.send(
+    '<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>Chat Service</title><meta name="description" content="Chat service one to one using socket.io by irufano"></head><body><h3>Chat service is running...</h3></body></html>'
+  );
+});
+
+socketIO.on("connection", (client) => {
+  //Get the chatID of the user and join in a room of the same chatID
+  chatID = client.handshake.query.chatID;
+  client.join(chatID);
+
+  //Leave the room if the user closes the socket
+  client.on("disconnect", () => {
+    client.leave(chatID);
+  });
+
+  //Send message to only a particular user
+  client.on("send_message", (message) => {
+    receiverChatID = message.receiverChatID;
+    senderChatID = message.senderChatID;
+    content = message.content;
+
+    //Send message to only that particular room
+    client.in(receiverChatID).emit("receive_message", {
+      content: content,
+      senderChatID: senderChatID,
+      receiverChatID: receiverChatID,
+    });
+  });
+});
+
+var port = process.env.PORT;
+
+server.listen(port, function (err) {
+  if (err) console.log(err);
+  console.log("Listening on port", port);
+});
